951479fb0ec20e75140056ab41a2abc24c4c3ca0
Solenoid <maruisansmai@gmail.com>
2020-03-25 16:50:59 +0900

    v-forによる反復作業の短縮化

--- 01_hello_vue/src/main.js
+++ 01_hello_vue/src/main.js
@@ -1,18 +1,30 @@
 let isClicked = false;
 const app = new Vue({
+  /* cssセレクタを指定するとそこがvueの機能を搭載する場所になる */
   el: "#app",
+
+  /* ウェブページに送信するデータを指定できます｡ページに表示したい文字やアトリビュートにurlやクラス名を送ることも可能 */
   data: {
     message: "Hello Vue",
     product: "Socks",
     isRecommended: true,
-    image: "./assets/vmSocks-green.jpg",
-    alt: "green socks image",
+    image: { src: "./assets/vmSocks-green.jpg", alt: "green socks image" },
+    inventory: 8,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        id: 1,
+        color: "green",
+      },
+      { id: 2, color: "blue" },
+    ],
     twitter: {
       url: "https://twitter.com/maruisannsimai",
       isClicked: isClicked,
     },
-    inventory: 8,
   },
+
+  /* イベントハンドラー */
   methods: {
     clickTwitterButton: () => {
       console.log(isClicked);
