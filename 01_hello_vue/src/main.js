04c78be8e9ee7d4d09f1ecce0493a06b2b05ff17
Solenoid <maruisansmai@gmail.com>
2020-03-26 21:07:26 +0900

    イベント,クラス,スタイル処理

--- 01_hello_vue/src/main.js
+++ 01_hello_vue/src/main.js
@@ -1,4 +1,3 @@
-let isClicked = false;
 const app = new Vue({
   /* cssセレクタを指定するとそこがvueの機能を搭載する場所になる */
   el: "#app",
@@ -11,24 +10,52 @@ const app = new Vue({
     image: { src: "./assets/vmSocks-green.jpg", alt: "green socks image" },
     inventory: 8,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    sizes: ["S", "M", "L", "XL"],
     variants: [
       {
         id: 1,
         color: "green",
+        image: { src: "./assets/vmSocks-green.jpg", alt: "green socks image" },
       },
-      { id: 2, color: "blue" },
+      {
+        id: 2,
+        color: "blue",
+        image: {
+          src: "./assets/vmSocks-blue-onWhite.jpg",
+          alt: "blue socks image",
+        },
+      },
     ],
+    cart: 0,
     twitter: {
       url: "https://twitter.com/maruisannsimai",
-      isClicked: isClicked,
+      isClicked: false,
+      class: "animated bounceOut",
     },
   },
-
   /* イベントハンドラー */
   methods: {
-    clickTwitterButton: () => {
-      console.log(isClicked);
-      isClicked = !isClicked;
+    clickTwitterButton() {
+      this.twitter.isClicked = true;
+      setInterval(() => {
+        this.twitter.isClicked = false;
+      }, 2000);
+    },
+    addToCart() {
+      this.cart++;
+    },
+    removeCart() {
+      if (this.cart <= 0) {
+        this.cart = 0;
+        return;
+      }
+      this.cart--;
+    },
+    resetCart() {
+      this.cart = 0;
+    },
+    updateProduct(imageObject) {
+      this.image = imageObject;
     },
   },
 });
