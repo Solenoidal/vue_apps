e6c603f80355b2bd647f8d032ace97fc1de25da0
Solenoid <maruisansmai@gmail.com>
2020-04-17 03:28:50 +0900

    追加 : モジュールの説明､real-world-appのルート情報

--- 02_real_world_app/src/router/index.js
+++ 02_real_world_app/src/router/index.js
@@ -1,6 +1,14 @@
+/*
+ルーターの制御を行うファイル 
+ルーターの情報(パス､名前､コンポーネント､エイリアス等)を格納し､
+App.vueで使うようにエクスポートしています
+*/
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
+import EventCreate from "../views/EventCreate.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +26,21 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/real-world-app",
+    name: "event-list",
+    component: EventList
+  },
+  {
+    path: "/real-world-app/event",
+    name: "event-show",
+    component: EventShow
+  },
+  {
+    path: "/real-world-app/create",
+    name: "event-create",
+    component: EventCreate
   }
 ];
 
