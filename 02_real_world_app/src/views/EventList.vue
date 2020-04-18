5d74d5be5f34092116dc9264b592e2c55f326b26
Solenoid <maruisansmai@gmail.com>
2020-04-19 07:32:52 +0900

    add/event component

--- 02_real_world_app/src/views/EventList.vue
+++ 02_real_world_app/src/views/EventList.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
     <h1>Events Listign</h1>
-    <div v-for="{ id } in events" :key="id" class="event">
-      <router-link :to="{ name: 'event-show', params: { id } }">
-        event #{{ id }}
-      </router-link>
+    <div v-for="event in events" :key="event.id" class="event">
+      <EventCard v-bind="event" />
     </div>
   </div>
 </template>
 
 <script>
+import EventCard from "@/components/EventCard.vue";
 export default {
+  components: { EventCard },
   data() {
     return {
-      events: [{ id: 1 }, { id: 2 }, { id: 3 }]
+      events: [
+        {
+          id: 1,
+          title: "JS-Conf 2020",
+          date: new Date(),
+          time: "11:20",
+          attendees: { id: "jfiaejif", name: "solenoid" }
+        },
+        { id: 2 },
+        { id: 3 }
+      ]
     };
   }
 };
 </script>
->
+
 <style>
 .event {
   display: inline-block;
