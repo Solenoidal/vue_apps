6f7d9c178cf55df8a54b2aa5a0a4e833bc8086ca
Solenoid <maruisansmai@gmail.com>
2020-04-19 13:11:41 +0900

    初めての状態管理

--- 02_real_world_app/src/store/index.js
+++ 02_real_world_app/src/store/index.js
@@ -4,7 +4,10 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: "ihaie", name: "solenoid" },
+    categories: ["food", "programming", "job", "sub culture"]
+  },
   mutations: {},
   actions: {},
   modules: {}
--- 02_real_world_app/src/views/EventCreate.vue
+++ 02_real_world_app/src/views/EventCreate.vue
@@ -1,5 +1,41 @@
 <template>
   <div>
-    <h1>Create new event</h1>
+    <!-- simple state usage is calling $state directly => {{$state.user.name}} -->
+    <h1>Create new event, {{ user.name }}</h1>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
+
+//
+<script>
+/* mapState is helper function processing state */
+import { mapState } from "vuex";
+export default {
+  // more useful way to use state is pass it to a method in computed property
+  // computed: {
+  //   userName() {
+  //     return this.$state.user.name;
+  //   }
+  // },
+
+  // or using mapState method
+  // computed: mapState({
+  //   username: state => state.user.name,
+  //   categories: "categories"
+  // }),
+
+  // if you want to ues mapState with other method, you can use spread operator
+  // computed:{
+  //   some_method(){
+  //     /* statement */
+  //     return
+  //   },
+  //   ...mapState(["user"])
+  // }
+
+  // the simplest state processing but it is used for the top-level state
+  computed: mapState(["user", "categories"])
+};
+</script>
