987fcbc8e5678790e88c338df9fb4c904f445c60
Solenoid <maruisansmai@gmail.com>
2020-04-19 13:45:10 +0900

    add/getters

--- 02_real_world_app/src/store/index.js
+++ 02_real_world_app/src/store/index.js
@@ -6,9 +6,19 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     user: { id: "ihaie", name: "solenoid" },
-    categories: ["food", "programming", "job", "sub culture"]
+    categories: ["food", "programming", "job", "sub culture"],
+    todos: [
+      { id: 1, title: "Buy a Python Book", done: false },
+      { id: 2, title: "Running", done: true },
+      { id: 3, title: "Clean my room", done: false }
+    ]
   },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => state.categories.length,
+    doneTodos: state => state.todos.filter(todo => todo.done),
+    activeTodosCount: state => state.todos.filter(todo => !todo.done).length
+  }
 });
--- 02_real_world_app/src/views/EventCreate.vue
+++ 02_real_world_app/src/views/EventCreate.vue
@@ -2,16 +2,23 @@
   <div>
     <!-- simple state usage is calling $state directly => {{$state.user.name}} -->
     <h1>Create new event, {{ user.name }}</h1>
+    <p>There are {{ catLength }} categories</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
+    <p>There are {{ activeTodosCount }} todos</p>
+    <div v-for="todo in todos" :key="todo.id">
+      <div>
+        {{ todo.title }}
+      </div>
+    </div>
   </div>
 </template>
 
 //
 <script>
 /* mapState is helper function processing state */
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 export default {
   // more useful way to use state is pass it to a method in computed property
   // computed: {
@@ -19,23 +26,20 @@ export default {
   //     return this.$state.user.name;
   //   }
   // },
-
+  //
   // or using mapState method
   // computed: mapState({
   //   username: state => state.user.name,
   //   categories: "categories"
   // }),
-
-  // if you want to ues mapState with other method, you can use spread operator
-  // computed:{
-  //   some_method(){
-  //     /* statement */
-  //     return
-  //   },
-  //   ...mapState(["user"])
-  // }
-
+  //
   // the simplest state processing but it is used for the top-level state
-  computed: mapState(["user", "categories"])
+  // computed: mapState(["user", "categories"])
+  //
+  // if you want to ues mapState with other method, you can use spread operator
+  computed: {
+    ...mapGetters(["catLength", "activeTodosCount"]),
+    ...mapState(["user", "categories", "todos"])
+  }
 };
 </script>
